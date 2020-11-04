8110db1e8ff056adbc96efc06796f59bf09a9764
Jason Leng <jleng@mit.edu>
2020-11-04 01:50:35 -0500

    Added conversion to RPY

--- src/getPose_test.cpp
+++ src/getPose_test.cpp
@@ -4,9 +4,14 @@
 #include "gazebo_msgs/ModelStates.h"
 #include "geometry_msgs/Twist.h"
 #include "geometry_msgs/Pose.h"
+#include "tf/transform_datatypes.h"
+#include "geometry_msgs/Vector3.h"
+#include "geometry_msgs/Quaternion.h"
+#include "tf/LinearMath/Matrix3x3.h"
 
 #define NUM_ROBOT 2
 #define OFFS 1      // because the first entry in modelstates is ground plane
+# define M_PI           3.14159265358979323846
 
 
 
@@ -18,10 +23,26 @@ void chatterCallback(const gazebo_msgs::ModelStates::ConstPtr& msg)
     // std::cout << msg->name.size() << std::endl;
 
     for(int i=OFFS; i<OFFS+NUM_ROBOT; i++)   {
-        std::cout << i << "-th position and orientation" << std::endl;
-        std::cout << msg->pose[i] << std::endl;
+        std::cout << msg->name[i] << " position and orientation" << std::endl;
+        std::cout << msg->pose[i].position << std::endl;
+
+        // convert to RPY
+        tf::Quaternion quat;
+        tf::quaternionMsgToTF(msg->pose[i].orientation, quat);
+
+        double roll, pitch, yaw;
+        tf::Matrix3x3(quat).getRPY(roll, pitch, yaw);
+
+        geometry_msgs::Vector3 rpy;
+        rpy.x = roll;
+        rpy.y = pitch;
+        rpy.z = yaw;
+
+        // std::cout << msg->name[i] << " yaw" << ": " << yaw * 180.0/M_PI << std::endl;
+
 
     }
+
 
 
 
